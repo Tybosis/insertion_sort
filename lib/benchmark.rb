eeba6823455a7649a677bdbb43ef3a4ab7a8191a
Tyler Pottle <tyler.pottle@gmail.com>
2015-04-02 12:37:39 -0700

    improve benchmarks to contrast with built in ruby sort method

--- lib/benchmark.rb
+++ lib/benchmark.rb
@@ -2,9 +2,16 @@ require 'benchmark'
 require_relative 'insertion'
 
 Benchmark.bm(3) do |x|
-  x.report("best:") { Sorting.insertion_sort((1..10_000).to_a) }
+  array = (1..10_000).to_a
+  x.report("insertion best  :") { Sorting.insertion_sort(array) }
 
-  x.report("worst:") { Sorting.insertion_sort((1..10_000).to_a.reverse) }
+  x.report("built-in best   :") { Sorting.insertion_sort(array) }
 
-  x.report("random:") { Sorting.insertion_sort((1..10_000).to_a.shuffle) }
+  x.report("insertion worst :") { Sorting.insertion_sort(array.reverse) }
+
+  x.report("built-in worst  :") { Sorting.insertion_sort(array.reverse) }
+
+  x.report("insertion random:") { Sorting.insertion_sort(array.shuffle) }
+
+  x.report("built-in random :") { Sorting.insertion_sort(array.shuffle) }
 end
